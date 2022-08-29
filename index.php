ff51f65f76ab8354973b68769ff8ce421a6071f6
moniruzzaman.saikat <saikat.moniruzzaman@gmail.com>
2022-08-29 23:38:58 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+// variables
+$name = "Moniruzzaman Saikat";  # with double quote
+$favMusician = 'Marshmello';    # with singel quote
+$age = 19;                      # integer
+$height = 5.7;                  # float
+
+echo $name;
